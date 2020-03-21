95a281d5dfbf26177d6d6362a1cdc27f0cfb64ef
Tin Danh Nguyen <t.nguyen@ibm.com>
2020-03-21 15:57:36 -0400

    Started implementing sb-lda.

--- wikipedia.py
+++ wikipedia.py
@@ -41,7 +41,7 @@ def main():
     W = len(vocab)
 
     # Initialize the algorithm with alpha=1/K, eta=1/K, tau_0=1024, kappa=0.7
-    olda = topicmodelvb.OnlineLDA(vocab, K, D, 1./K, 0.01, 1024., 0.7)
+    olda = topicmodelvb.TopicModel(vocab, K, D, 1./K, 0.01, 1024., 0.7)
     # Run until we've seen D documents. (Feel free to interrupt *much*
     # sooner than this.)
     for iteration in range(0, max_iter):
